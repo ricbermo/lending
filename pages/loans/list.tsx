233a080bf44a0bc67f7619a85eeaaf95a983e6c2
Eric Vargas <ericvvc9@gmail.com>
2023-05-08 12:02:00 -0500

    Add list of users

--- pages/loans/list.tsx
+++ pages/loans/list.tsx
@@ -8,7 +8,6 @@ const [loans, loading] = useCollection(collection(getFirestore(app), 'loans'),);
   if (!loading && loans) {
     loans.docs.map((doc) => console.log(doc.data()));
   }
-
   return (
     <p>open console</p>
   )
